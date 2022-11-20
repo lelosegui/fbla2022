74068fca16565b465acf14fb758e2876a9d64141
Laura Elosegui <laura@elosegui.me>
2022-11-20 18:57:48 -0500

    made left and right, changed to pink, added max speed and friction

--- src/js/block.js
+++ src/js/block.js
@@ -1,7 +1,8 @@
 class Block{
     constructor(x,y,width,high) {
 
-        this.speed = 0;
+        this.ySpeed = 0;
+        this.xSpeed = 0;
         this.acceleration = 0.367;
         this.friction = 0.05;
         this.maxSpeed = 3;
@@ -21,7 +22,7 @@ class Block{
 
 
         context.beginPath();
-        context.fillStyle = 'black';
+        context.fillStyle = 'HotPink';
         context.fillRect(this.x,this.y,this.width,this.high);
 
         context.fill();
@@ -30,31 +31,56 @@ class Block{
     #move(){
 
         if(this.controls.forward===true){
-            this.speed += this.acceleration;
+            this.ySpeed += this.acceleration;
         }
 
         if(this.controls.reverse===true){
-            this.speed -= this.acceleration;
+            this.ySpeed -= this.acceleration;
         }
 
-        if(this.speed >=this.maxSpeed){
-            this.speed = this.maxSpeed;
+        if(this.controls.left===true){
+            this.xSpeed -= this.acceleration;
         }
 
-        if(this.speed <= -this.maxSpeed){
-            this.speed = -this.maxSpeed;
+        if(this.controls.right===true){
+            this.xSpeed += this.acceleration;
         }
 
-        if(this.speed<0){
-            this.speed += this.friction;
+        if(this.ySpeed >=this.maxSpeed){
+            this.ySpeed = this.maxSpeed;
         }
 
-        if(this.speed>0){
-            this.speed -= this.friction;
+        if(this.ySpeed <= -this.maxSpeed){
+            this.ySpeed = -this.maxSpeed;
         }
 
-        this.y=this.y-this.speed;
+        if(this.xSpeed >=this.maxSpeed){
+            this.xSpeed = this.maxSpeed;
+        }
 
+        if(this.xSpeed <= -this.maxSpeed){
+            this.xSpeed = -this.maxSpeed;
+        }
+
+        if(this.ySpeed<0){
+            this.ySpeed += this.friction;
+        }
+
+        if(this.ySpeed>0){
+            this.ySpeed -= this.friction;
+        }
+
+        if(this.xSpeed<0){
+            this.xSpeed += this.friction;
+        }
+
+        if(this.xSpeed>0){
+            this.xSpeed -= this.friction;
+        }
+
+
+        this.y -= this.ySpeed;
+        this.x += this.xSpeed;
     }
 }
 

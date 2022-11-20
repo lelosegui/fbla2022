df8e49147dff869edeffaad64ebfc3fb25f01212
Laura Elosegui <laura@elosegui.me>
2022-11-20 16:54:24 -0500

    project structure

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("playground");
+
+//canvas.width = 1900;
+
+const context=canvas.getContext("2d");
+
+
+function animate() {
+    canvas.height = window.innerHeight;
+}
+
+animate();
+
+//oh
